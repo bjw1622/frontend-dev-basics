6a36fccb9f89c8075475dfd1812c482d3b4b5c45
백재원 <bjw1622@gmail.com>
2023-10-17 15:22:04 +0900

    feat : js pratice

---
+++ javascript-pratices/basics/ex01.js
@@ -0,0 +1,4 @@
+console.log("test");
+console.info("test");
+console.warn("test");
+console.error("test");
---
+++ javascript-pratices/basics/ex02.js
@@ -0,0 +1,15 @@
+/**
+ * 변수와 자료형
+ *
+ * 기본 타입
+ *  - undefined
+ *  - number
+ *  - string
+ *  - boolean
+ *
+ * 객체 타입
+ *
+ * function
+ */
+
+console.log("=== 기본타입() ======");
