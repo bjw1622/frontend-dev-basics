0449ec36bab20337c2b6d433a0682ed1bd9a181d
백재원 <bjw1622@gmail.com>
2023-10-27 09:52:14 +0900

    feat : 구조 분해 할당

---
+++ javascript-pratices/es6/ex06.js
@@ -0,0 +1,11 @@
+// 구조분해할당
+
+const user = {
+  firstName: "둘",
+  lastName: "리",
+  email: "bjw1622@gmail.com",
+};
+// 변수를 정의한다
+const { firstName, lastName, email } = user;
+
+console.log(firstName, lastName, email);
